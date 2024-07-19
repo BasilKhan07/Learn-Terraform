f9ce30fb323272d500e579a2e2ae428f490b8fe2
BasilKhan07 <basilkhan06@gmail.com>
2024-07-19 22:04:20 +0500

    Creating security group

---
+++ 18 security-group-ec2-instance/instance.tf
@@ -0,0 +1,51 @@
+# resource "aws_key_pair" "key-tf" {
+#   key_name   = "key-tf"
+#   public_key = file("${path.module}/id_rsa.pub")
+# }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-0862be96e41dcbf74"
+#   instance_type = "t2.micro"
+#   key_name = "${aws_key_pair.key-tf.key_name}"
+#   tags = {
+#     Name = "first-tf-instance"
+#   }
+# }
