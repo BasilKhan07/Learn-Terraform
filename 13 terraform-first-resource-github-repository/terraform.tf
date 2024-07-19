d9c61e8ac03dbd1d7669be46dd3b2cd383b96c9a
BasilKhan07 <basilkhan06@gmail.com>
2024-07-19 19:04:36 +0500

    creating github repo using terraform

---
+++ 13 terraform-first-resource-github-repository/terraform.tf
@@ -0,0 +1,10 @@
+provider "github" {
+  token = "get your token from github account"
+}
+
+resource "github_repository" "terraform-first-repo" {
+  name        = "first-repo-from-terraform"
+  description = "My first github resource for learning terraform"
+  visibility = "public"
+  auto_init = true
+}
